e92e460f5e5b522282808cba7b2456f5f74aaf6a
Peter Esselius <pepp@me.com>
2025-04-16 20:43:11 +0200

    actions.nix

--- flake.nix
+++ flake.nix
@@ -3,6 +3,7 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     flake-parts.url = "github:hercules-ci/flake-parts";
     git-hooks-nix.url = "github:cachix/git-hooks.nix";
+    actions-nix.url = "github:nialov/actions.nix";
   };
 
   outputs = inputs@{ flake-parts, ... }: flake-parts.lib.mkFlake { inherit inputs; } {
@@ -10,6 +11,7 @@
 
     imports = [
       inputs.git-hooks-nix.flakeModule
+      inputs.actions-nix.flakeModules.actions-nix
     ];
 
     perSystem = { config, ... }: {
@@ -19,6 +21,27 @@
         statix.enable = true;
         deadnix.enable = true;
         flake-checker.enable = true;
+      };
+    };
+
+    flake.actions-nix = {
+      pre-commit.enable = true;
+      workflows = {
+        ".github/workflows/main.yaml" = {
+          jobs = {
+            nix-flake-check = {
+              runs-on = "ubuntu-latest";
+              timeout-minutes = 60;
+              steps = [
+                { uses = "actions/checkout@v4"; }
+
+                inputs.actions-nix.lib.steps.DeterminateSystemsNixInstallerAction
+
+                { run = "nix -Lv flake check"; }
+              ];
+            };
+          };
+        };
       };
     };
   };
