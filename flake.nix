1b53663211e71c339c1645f4596476ce9dabaf35
Peter Esselius <pepp@me.com>
2025-04-17 22:08:00 +0200

    flink overlay

--- flake.nix
+++ flake.nix
@@ -24,35 +24,8 @@
         overlays = [
           (_: prev: {
             zeppelin = prev.callPackage ./pkgs/zeppelin.nix { };
-            flink = prev.flink.overrideAttrs (old: rec {
-              pname = "flink";
-              version = "1.17.1";
-
-              src = pkgs.fetchurl {
-                url = "mirror://apache/flink/${pname}-${version}/${pname}-${version}-bin-scala_2.12.tgz";
-                sha256 = "sha256-HpVDS3ydi2Z1SINAUed9lni9i8FCr0SI8yBCYP4wxyM=";
-              };
-
-              installPhase =
-                let
-                  flinkTableApiScalaBridge = pkgs.fetchurl {
-                    url = "mirror://maven/org/apache/flink/flink-table-api-scala-bridge_2.12/${version}/flink-table-api-scala-bridge_2.12-${version}.jar";
-                    sha256 = "sha256-AphJidco/ws1bYS70vIai48qdGj7xqeDGST+kBekTRM=";
-                  };
-                  flinkTableApiScala = pkgs.fetchurl {
-                    url = "mirror://maven/org/apache/flink/flink-table-api-scala_2.12/${version}/flink-table-api-scala_2.12-${version}.jar";
-                    sha256 = "sha256-aY+J+ShZ+ubaFRk/cZzNsPJO1M0KvMUsuSAGZEnatXk=";
-                  };
-                in
-                old.installPhase + ''
-                  mv $out/opt/flink/opt/flink-sql-client* $out/opt/flink/lib/
-                  mv $out/opt/flink/opt/flink-table-planner* $out/opt/flink/lib/
-                  mv $out/opt/flink/lib/flink-table-planner-loader* $out/opt/flink/opt/
-                  ln -s ${flinkTableApiScalaBridge} $out/opt/flink/lib/
-                  ln -s ${flinkTableApiScala} $out/opt/flink/lib/
-                '';
-            });
           })
+          (import ./flink-overlay.nix)
         ];
       };
 
