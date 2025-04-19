d837a7a4ce1af20226ff26b2ac516c1600837b06
Peter Esselius <peter.esselius@pagero.com>
2025-04-19 16:19:10 +0200

    zeppelin stdout logging

--- services/zeppelin.nix
+++ services/zeppelin.nix
@@ -1,9 +1,17 @@
 { config, lib, pkgs, ... }:
+
+let
+  cfg = config.services.zeppelin;
+in
 {
   options = {
     services.zeppelin = {
       enable = lib.mkEnableOption "Enable hello service";
       package = lib.mkPackageOption pkgs "zeppelin" { };
+      confDir = lib.mkOption {
+        type = lib.types.path;
+        default = pkgs.writeTextDir "log4j.properties" cfg.log4jProperties;
+      };
       logDir = lib.mkOption {
         type = lib.types.str;
         default = ".data/zeppelin/logs";
@@ -28,25 +36,33 @@
       flinkPkg = lib.mkPackageOption pkgs "flink_1_17" { };
       useSpark = lib.mkEnableOption "Enable spark support";
       sparkPkg = lib.mkPackageOption pkgs "spark_3_5" { };
-    };
-  };
-  config =
-    let
-      cfg = config.services.zeppelin;
-    in
-    lib.mkIf cfg.enable {
-      settings.processes.zeppelin = {
-        command = "${cfg.package}/bin/zeppelin.sh";
-        environment = [
-          "ZEPPELIN_LOG_DIR=${cfg.logDir}"
-          "ZEPPELIN_PID_DIR=${cfg.pidDir}"
-          "ZEPPELIN_NOTEBOOK_DIR=$${PWD}"
-          "ZEPPELIN_CONFIG_FS_DIR=$${PWD}/.data/zeppelin/conf"
-          "ZEPPELIN_WAR_TEMPDIR=$${PWD}/.data/zeppelin/tmp"
-          "ZEPPELIN_PORT=${toString cfg.port}"
-          "USE_HADOOP=false"
-        ] ++ lib.optional cfg.useFlink "FLINK_HOME=${cfg.flinkPkg}/opt/flink"
-        ++ lib.optional cfg.useSpark "SPARK_HOME=${cfg.sparkPkg}";
+
+      log4jProperties = lib.mkOption {
+        type = lib.types.str;
+        default = ''
+          log4j.rootLogger = INFO, stdout
+
+          log4j.appender.stdout = org.apache.log4j.ConsoleAppender
+          log4j.appender.stdout.layout = org.apache.log4j.PatternLayout
+          log4j.appender.stdout.layout.ConversionPattern=%5p [%d] ({%t} %F[%M]:%L) - %m%n
+        '';
       };
     };
+  };
+  config = lib.mkIf cfg.enable {
+    settings.processes.zeppelin = {
+      command = "${cfg.package}/bin/zeppelin.sh";
+      environment = [
+        "ZEPPELIN_LOG_DIR=${cfg.logDir}"
+        "ZEPPELIN_PID_DIR=${cfg.pidDir}"
+        "ZEPPELIN_NOTEBOOK_DIR=$${PWD}"
+        "ZEPPELIN_CONF_DIR=${cfg.confDir}"
+        "ZEPPELIN_CONFIG_FS_DIR=$${PWD}/.data/zeppelin/conf"
+        "ZEPPELIN_WAR_TEMPDIR=$${PWD}/.data/zeppelin/tmp"
+        "ZEPPELIN_PORT=${toString cfg.port}"
+        "USE_HADOOP=false"
+      ] ++ lib.optional cfg.useFlink "FLINK_HOME=${cfg.flinkPkg}/opt/flink"
+      ++ lib.optional cfg.useSpark "SPARK_HOME=${cfg.sparkPkg}";
+    };
+  };
 }
